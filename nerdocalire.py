bfb069a8bf8b38d043b7282458281ebdddc1d6d1
UnProgrammatore <andrea.appstore@gmail.com>
2021-11-04 23:12:22 +0100

    A bunch of random stuff github copilot wrote (and I added a little bit to)

--- nerdocalire.py
+++ nerdocalire.py
@@ -3,6 +3,8 @@ from telegram.ext import CommandHandler
 import logging
 import mariadb
 import os
+import requests
+import random
 
 token = os.environ['NERDOCALIREBOT_TOKEN']
 mariadbUser = os.environ['NERDOCALIREBOT_MARIADB_USER']
@@ -200,9 +202,149 @@ def skarma(update, context):
         conn.close()
 def ping(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=f'/pong')
-    
 
-    
+def yell(update, context):
+    conn = connect_database(update, context)
+    try:
+        user = update.message.from_user
+        cur = conn.cursor()
+        cur.execute("SELECT name FROM nerdocalissiani WHERE telegramUserId = ?", (user.id,))
+        val = cur.fetchone()
+        if val[0] == None:
+            nome = "uno sconosciuto"
+        else:
+            nome = val[0]
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f'{nome} ha fatto una urla')
+    finally:
+        conn.close()
+
+def getAllTransactionsFromToday(update, context):
+    conn = connect_database(update, context)
+    try:
+        user = update.message.from_user
+        cur = conn.cursor()
+        cur.execute("SELECT t.nerdocalire, t.reason, t.tdate FROM transactions t INNER JOIN nerdocalissiani n on t.nerdocalissianoId = n.nerdocalissianoId WHERE telegramUserId = ? AND tdate > UTC_TIMESTAMP() - INTERVAL 1 DAY ORDER BY transactionId DESC", (user.id,))
+        all = cur.fetchall()
+        s = "Queste sono le tue ultime 10 transazioni di oggi:"
+        for (nerdocalire, reason, date) in all:
+            s += f'\nIl giorno {date} hai {"speso" if nerdocalire < 0 else "ottenuto"} {abs(nerdocalire)} nerdocalire perché {reason}'
+        context.bot.send_message(chat_id=update.effective_chat.id, text=s)
+    finally:
+        conn.close()
+
+def getRichestUser(update, context):
+    conn = connect_database(update, context)
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT name, karma FROM nerdocalissiani ORDER BY karma DESC LIMIT 1")
+        res = cur.fetchone()
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f'Il nerdocalissiano più ricco è {res[0]} con {res[1]} karma')
+    finally:
+        conn.close()
+
+def leave(update, context):
+    conn = connect_database(update, context)
+    try:
+        user = update.message.from_user
+        cur = conn.cursor()
+        cur.execute("DELETE FROM nerdocalissiani WHERE telegramUserId = ?", (user.id,))
+        if cur.rowcount > 0:
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f'Hai lasciato il gruppo nerdocalissiani')
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f'Non sei registrato')
+    finally:
+        conn.close()
+
+# Undoes the last transaction of the user by creating a new transaction with the opposite sign
+def undoLastTransaction(update, context):
+    conn = connect_database(update, context)
+    try:
+        user = update.message.from_user
+        cur = conn.cursor()
+        cur.execute("SELECT t.nerdocalire, t.reason, t.tdate FROM transactions t INNER JOIN nerdocalissiani n on t.nerdocalissianoId = n.nerdocalissianoId WHERE telegramUserId = ? ORDER BY transactionId DESC LIMIT 1", (user.id,))
+        res = cur.fetchone()
+        if res == None:
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f'Non hai ancora speso niente')
+        else:
+            cur.execute("INSERT INTO transactions (nerdocalissianoId, nerdocalire, reason, tdate) VALUES ((SELECT nerdocalissianoId FROM nerdocalissiani WHERE telegramUserId = ?), ?, ?, ?)", (user.id, -res[0], res[1], res[2]))
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f'Hai annullato l\'ultima transazione')
+    finally:
+        conn.close()
+
+# If the user is an admin, gives all of the karma from the other users to the admin
+def imTheBoss(update, context):
+    conn = connect_database(update, context)
+    try:
+        user = update.message.from_user
+        cur = conn.cursor()
+        cur.execute("SELECT admin FROM nerdocalissiani WHERE telegramUserId = ?", (user.id,))
+        val = cur.fetchone()
+        if val[0] == 1:
+            cur.execute("SELECT telegramUserId, karma FROM nerdocalissiani WHERE telegramUserId <> ?", (user.id,))
+            for (telegramUserId, karma) in cur.fetchall():
+                cur.execute("UPDATE nerdocalissiani SET karma = karma + ? WHERE telegramUserId = ?", (karma, user.id))
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f'Hai preso tutto il karma da tutti gli altri nerdocalissiani')
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f'Non sei admin')
+    finally:
+        conn.close()
+
+# Writes 10 consecutive messages, counting from 1 to 10
+def countToTen(update, context):
+    conn = connect_database(update, context)
+    try:
+        user = update.message.from_user
+        cur = conn.cursor()
+        cur.execute("SELECT name FROM nerdocalissiani WHERE telegramUserId = ?", (user.id,))
+        val = cur.fetchone()
+        if val[0] == None:
+            nome = "uno sconosciuto"
+        else:
+            nome = val[0]
+        for i in range(1, 11):
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f'{nome} ha scritto {i}')
+    finally:
+        conn.close()
+# Looks up a random meme on the internet and sends it
+def meme(update, context):
+    r = requests.get("https://meme-api.herokuapp.com/gimme")
+    meme = r.json()
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=meme['url'])
+
+# Sends a random quote from the internet
+def quote(update, context):
+    r = requests.get("https://api.quotable.io/random")
+    quote = r.json()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f'{quote["content"]} - {quote["author"]}')
+
+# Rolls a dice with the specified number of sides
+def rollDice(update, context):
+    conn = connect_database(update, context)
+    try:
+        user = update.message.from_user
+        cur = conn.cursor()
+        cur.execute("SELECT name FROM nerdocalissiani WHERE telegramUserId = ?", (user.id,))
+        val = cur.fetchone()
+        if val[0] == None:
+            nome = "uno sconosciuto"
+        else:
+            nome = val[0]
+        num = random.randint(1, int(context.args[0]))
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f'{nome} ha tirato {num}')
+    finally:
+        conn.close()
+
+# sends a picture of a cat
+def cat(update, context):
+    r = requests.get("https://api.thecatapi.com/v1/images/search")
+    cat = r.json()[0]['url']
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=cat)
+
+# sends a picture of a dog
+def dog(update, context):
+    r = requests.get("https://dog.ceo/api/breeds/image/random")
+    dog = r.json()['message']
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=dog)
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
 updater = Updater(token = token, use_context = True)
